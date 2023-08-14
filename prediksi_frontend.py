e0215dbe7d5b745b07fd10b04dfd910d062f2fba
mariooctavunikom <141686984+mariooctavunikom@users.noreply.github.com>
2023-08-14 08:00:20 +0700

    Update prediksi_frontend.py

--- prediksi_frontend.py
+++ prediksi_frontend.py
@@ -6,7 +6,6 @@ diabetes_model = pickle.load(open('diabetes_model.sav' , 'rb'))
 
 # Judul Halaman
 st.title('Cek Prediksi Diabetes')
-st.text ('Model by: Mario - 10217035')
 
 # Kolom Form
 col1, col2 = st.columns(2)
